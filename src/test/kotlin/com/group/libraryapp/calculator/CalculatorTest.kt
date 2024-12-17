9a7abd76ee90ed66728073ddec101d34d32baaf7
leedanbi <leedanbiii@gentlemonster.com>
2024-12-17 16:31:47 +0900

    Clean up tracked files and apply .gitignore

---
+++ src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
@@ -0,0 +1,23 @@
+package com.group.libraryapp.calculator
+
+fun main() {
+    val calculatorTest = CalculatorTest()
+    calculatorTest.addTest()
+}
+
+class CalculatorTest {
+
+    fun addTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        calculator.add(3)
+
+        // then
+        if (calculator.number != 8) {
+            throw IllegalArgumentException()
+        }
+
+    }
+}
